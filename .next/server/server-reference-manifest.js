e432cf62b9f0bfeaac6a1404f01c5c243780173b
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-02-07 21:26:18 +0500

    wrapper component developed

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mh8AHY9p2S34L3P4Ghy37ttmSQw5pcbyT8BRmH9KNo4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xaaJZET+qITV1snZnk/cOSylQkIYZKn8TMRuauynLnQ=\"\n}"
