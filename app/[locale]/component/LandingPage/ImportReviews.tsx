83a1d9747375b065220821d1457e7fd3a6766f92
gulnabi-afridi <109666507+gulnabi-afridi@users.noreply.github.com>
2024-02-10 15:58:27 +0500

    animation for import reviews section added

--- app/[locale]/component/LandingPage/ImportReviews.tsx
+++ app/[locale]/component/LandingPage/ImportReviews.tsx
@@ -1,135 +1,184 @@
+'use client';
+
 import React from 'react';
 import Wrapper from '../Shared/ComponentWrapper';
 import * as Icons from '../../../../svg/Icons';
 import Image from 'next/image';
+import { Fade, Slide, Zoom } from 'react-awesome-reveal';
 
 const ImportReviews: React.FC = () => {
   return (
     <div className='w-full py-10 bg-white-1'>
       <Wrapper>
         <div className='flex flex-col gap-8 w-full justify-center items-center'>
-          <div className='flex flex-col gap-2 csm:gap-4 w-full justify-center items-center'>
-            <p className='text-[20px] csm:text-[25px] md:text-[30px] lg:text-[40px] text-black-1 font-semibold'>
-              Import existing reviews,{' '}
-              <span className='uppercase text-[12px] sm:text-[16px] csm:text-[22px] md:text-[28px] lg:text-[35px] text-[#534599] font-rockSalt'>
-                once click
-              </span>
-            </p>
-            <p className='text-[16px] csm:text-[18px] md:text-[20px] max-w-[550px] w-full text-center text-black-3 font-normal'>
-              Our advanced import module simplifies the processof importing
-              reviews from other platforms, saving you time and hassle.
-            </p>
-            {/* try it now ---->  */}
-            <div className='flex justify-center items-center gap-2'>
-              <p className='text-[12px] whitespace-nowrap sm:text-[16px] csm:text-[18px] md:text-[20px] lg:text-[24px] text-black-2 font-medium'>
-                Try it now
+          <Fade triggerOnce className='w-full'>
+            <div className='flex flex-col gap-2 csm:gap-4 w-full justify-center items-center'>
+              <p className='text-[20px] csm:text-[25px] md:text-[30px] lg:text-[40px] text-black-1 font-semibold'>
+                Import existing reviews,{' '}
+                <span className='uppercase text-[12px] sm:text-[16px] csm:text-[22px] md:text-[28px] lg:text-[35px] text-[#534599] font-rockSalt'>
+                  once click
+                </span>
               </p>
-              <p className='text-[12px] whitespace-nowrap sm:text-[16px] csm:text-[18px] md:text-[20px] lg:text-[24px] text-[#F38B94] bg-[#FEECEE] px-[6px] md:px-[8px] py-[3px] md:py-[4px] rounded-[4px] font-medium'>
-                20 free imports
+              <p className='text-[16px] csm:text-[18px] md:text-[20px] max-w-[550px] w-full text-center text-black-3 font-normal'>
+                Our advanced import module simplifies the process of importing
+                reviews from other platforms, saving you time and hassle.
               </p>
-              <Icons.rightArrow className='w-[24px] h-[24px]' fill='#33363F' />
-              <p className='text-[12px] whitespace-nowrap sm:text-[16px] csm:text-[18px] md:text-[20px] lg:text-[24px] text-[#15B1FE] font-medium'>
-                20 free imports
-              </p>
-              <Icons.bulb className='w-[20px] lg:w-[24px] h-[20px] lg:h-[25px]' />
-            </div>
-          </div>
-          {/* import reviews --->  */}
-          <div className='w-full flex flex-col gap-2 csm:gap-4 justify-center items-center'>
-            <div className='w-full max-w-[580px] md:gap-0 gap-3 md:max-w-[880px] p-3 sm:p-5 lg:p-6 flex md:flex-row flex-col justify-between items-start csm:items-center rounded-[15px] bg-[#D0D8FF]'>
-              {/* left portion --->  */}
-              <div className='flex gap-3 justify-center items-start'>
-                <div className='w-[80px] csm:w-[100px] h-[80px] csm:h-[100px] relative'>
-                  <Image
-                    src='/assets/profile.svg'
-                    fill
-                    alt=''
-                    className='object-fill'
+              {/* try it now ---->  */}
+              <Fade delay={100} triggerOnce>
+                <div className='flex justify-center items-center gap-2'>
+                  <p className='text-[12px] whitespace-nowrap sm:text-[16px] csm:text-[18px] md:text-[20px] lg:text-[24px] text-black-2 font-medium'>
+                    Try it now
+                  </p>
+                  <p className='text-[12px] whitespace-nowrap sm:text-[16px] csm:text-[18px] md:text-[20px] lg:text-[24px] text-[#F38B94] bg-[#FEECEE] px-[6px] md:px-[8px] py-[3px] md:py-[4px] rounded-[4px] font-medium'>
+                    20 free imports
+                  </p>
+                  <Icons.rightArrow
+                    className='w-[24px] h-[24px]'
+                    fill='#33363F'
                   />
-                </div>
-                <div className='flex flex-col gap-3 items-start'>
-                  <p className='text-[18px] csm:text-[20px] lg:text-[24px] text-black-1 font-medium'>
-                    Charlie Robbbbieee
+                  <p className='text-[12px] whitespace-nowrap sm:text-[16px] csm:text-[18px] md:text-[20px] lg:text-[24px] text-[#15B1FE] font-medium'>
+                    20 free imports
                   </p>
-                  <div className='flex justify-center items-center gap-2 csm:gap-3'>
-                    {/* send review icon -->  */}
-                    <button className='flex hover:opacity-80 justify-center rounded-md bg-[#534599] items-center gap-1 lg:gap-2 px-[10px] csm:px-[14px] lg:px-[16px] py-[8px]'>
-                      <Icons.clickArrow className='w-[18px] csm:w-[24px] h-[18px] csm:h-[24px]' />
-                      <p className='text-[12px] csm:text-[16px] lg:text-[18px] text-white-1 font-medium'>
-                        Send review
-                      </p>
-                    </button>
-                    {/* share icon --->  */}
-                    <button className='hover:opacity-80 px-[10px] py-[8px] bg-white-1 rounded-[4px] flex justify-center items-center'>
-                      <Icons.share className='w-[20px] csm:w-[25px] h-[20px] csm:h-[25px]' />
-                    </button>
-                    {/* report button ---->  */}
-                    <button className='csm:flex hidden bg-white-1 rounded-md px-[14px] lg:px-[16px] py-[8px] justify-center items-center gap-2'>
-                      <Icons.report className='w-[20px] csm:w-[25px] h-[20px] csm:h-[25px]' />
-                      <p className='text-black-2 text-[12px] csm:text-[18px] font-medium'>
-                        Report
-                      </p>
-                    </button>
+                  <Icons.bulb className='w-[20px] lg:w-[24px] h-[20px] lg:h-[25px]' />
+                </div>
+              </Fade>
+            </div>
+          </Fade>
+          {/* import reviews --->  */}
+          <Slide direction='up' triggerOnce className='w-full'>
+            <div className='w-full flex flex-col gap-2 csm:gap-4 justify-center items-center'>
+              <Zoom
+                triggerOnce
+                className='w-full flex justify-center items-center'
+              >
+                <div className='w-full max-w-[580px] md:gap-0 gap-3 md:max-w-[880px] p-3 sm:p-5 lg:p-6 flex md:flex-row flex-col justify-between items-start csm:items-center rounded-[15px] bg-[#D0D8FF]'>
+                  {/* left portion --->  */}
+                  <div className='flex gap-3 justify-center items-start'>
+                    <Fade triggerOnce>
+                      <div className='w-[80px] csm:w-[100px] h-[80px] csm:h-[100px] relative'>
+                        <Image
+                          src='/assets/profile.svg'
+                          fill
+                          alt=''
+                          className='object-fill'
+                        />
+                      </div>
+                    </Fade>
+                    <Fade cascade damping={0.5} triggerOnce>
+                      <div className='flex flex-col gap-3 items-start'>
+                        <p className='text-[18px] csm:text-[20px] lg:text-[24px] text-black-1 font-medium'>
+                          Charlie Robbbbieee
+                        </p>
+                        <div className='flex justify-center items-center gap-2 csm:gap-3'>
+                          {/* send review icon -->  */}
+                          <Slide direction='left' triggerOnce>
+                            <button className='flex hover:opacity-80 justify-center rounded-md bg-[#534599] items-center gap-1 lg:gap-2 px-[10px] csm:px-[14px] lg:px-[16px] py-[8px]'>
+                              <Icons.clickArrow className='w-[18px] csm:w-[24px] h-[18px] csm:h-[24px]' />
+                              <p className='text-[12px] csm:text-[16px] lg:text-[18px] text-white-1 font-medium'>
+                                Send review
+                              </p>
+                            </button>
+                          </Slide>
+                          {/* share icon --->  */}
+                          <Slide direction='right' triggerOnce>
+                            <button className='hover:opacity-80 px-[10px] py-[8px] bg-white-1 rounded-[4px] flex justify-center items-center'>
+                              <Icons.share className='w-[20px] csm:w-[25px] h-[20px] csm:h-[25px]' />
+                            </button>
+                          </Slide>
+                          {/* report button ---->  */}
+                          <Fade triggerOnce>
+                            <button className='csm:flex hidden bg-white-1 rounded-md px-[14px] lg:px-[16px] py-[8px] justify-center items-center gap-2'>
+                              <Icons.report className='w-[20px] csm:w-[25px] h-[20px] csm:h-[25px]' />
+                              <p className='text-black-2 text-[12px] csm:text-[18px] font-medium'>
+                                Report
+                              </p>
+                            </button>
+                          </Fade>
+                        </div>
+                      </div>
+                    </Fade>
                   </div>
+                  {/* import button ----> */}
+                  <Zoom triggerOnce>
+                    <div className='flex justify-center items-center gap-2'>
+                      <button className='flex justify-center bg-[#534599] rounded-md items-center gap-2 px-[18px] hover:opacity-80 py-[8px]'>
+                        <Icons.star className='w-[16px] csm:w-[19px] h-[16px] csm:h-[18px]' />
+                        <p className='text-white-1 text-[12px] csm:text-[16px] lg:text-[18px] font-medium'>
+                          Import
+                        </p>
+                      </button>
+                      {/* report button for small screen  ---->  */}
+                      <button className='flex csm:hidden bg-white-1 rounded-md px-[14px] lg:px-[16px] py-[8px] justify-center items-center gap-2'>
+                        <Icons.report className='w-[20px] csm:w-[25px] h-[20px] csm:h-[25px]' />
+                        <p className='text-black-2 text-[12px] csm:text-[18px] font-medium'>
+                          Report
+                        </p>
+                      </button>
+                    </div>
+                  </Zoom>
                 </div>
-              </div>
-              {/* import button ----> */}
-              <div className='flex justify-center items-center gap-2'>
-                <button className='flex justify-center bg-[#534599] rounded-md items-center gap-2 px-[18px] hover:opacity-80 py-[8px]'>
-                  <Icons.star className='w-[16px] csm:w-[19px] h-[16px] csm:h-[18px]' />
-                  <p className='text-white-1 text-[12px] csm:text-[16px] lg:text-[18px] font-medium'>
-                    Import
-                  </p>
-                </button>
-                {/* report button for small screen  ---->  */}
-                <button className='flex csm:hidden bg-white-1 rounded-md px-[14px] lg:px-[16px] py-[8px] justify-center items-center gap-2'>
-                  <Icons.report className='w-[20px] csm:w-[25px] h-[20px] csm:h-[25px]' />
-                  <p className='text-black-2 text-[12px] csm:text-[18px] font-medium'>
-                    Report
-                  </p>
-                </button>
+              </Zoom>
+              {/* reviews --->  */}
+              <div className='flex flex-col gap-1 w-full justify-center items-center'>
+                {/* upwork review -->  */}
+                <Slide
+                  direction='left'
+                  triggerOnce
+                  className='w-full flex justify-center items-center'
+                >
+                  <div className='w-full max-w-[320px] csm:max-w-[480px] md:max-w-[720px] bg-[#F3F5F7] rounded-[10px] p-2 csm:p-3 md:p-4 flex justify-between items-center'>
+                    <div className='flex justify-center items-center gap-2 csm:gap-3'>
+                      <Icons.upwork className='w-[30px] csm:w-[48px] h-[30px] csm:h-[48px]' />
+                      <p className='text-[14px] csm:text-[18px] text-black-2 font-medium'>
+                        Found 20 reviews
+                      </p>
+                    </div>
+                    <p className='text-[14px] csm:text-[18px] text-[#777980] font-medium'>
+                      importing...
+                    </p>
+                  </div>
+                </Slide>
+                {/* fiver review ---> */}
+                <Fade damping={0.5} triggerOnce className='w-full'>
+                  <Zoom
+                    fraction={0.1}
+                    triggerOnce
+                    className='w-full flex justify-center items-center'
+                  >
+                    <div className='w-full max-w-[300px] csm:max-w-[460px] md:max-w-[700px] bg-[#F3F5F7] rounded-[10px] p-3 md:p-4 flex justify-between items-center'>
+                      <div className='flex justify-center items-center gap-2 csm:gap-3'>
+                        <Icons.fiver className='w-[30px] csm:w-[48px] h-[30px] csm:h-[48px]' />
+                        <p className='text-[14px] csm:text-[18px] text-black-2 font-medium'>
+                          Found 20 reviews
+                        </p>
+                      </div>
+                      <p className='text-[14px] csm:text-[18px] text-[#777980] font-medium'>
+                        importing...
+                      </p>
+                    </div>
+                  </Zoom>
+                </Fade>
+                {/* google review ---> */}
+                <Slide
+                  direction='right'
+                  triggerOnce
+                  className='w-full flex justify-center items-center'
+                >
+                  <div className='w-full max-w-[280px] csm:max-w-[440px] md:max-w-[680px] bg-[#F3F5F7] rounded-[10px] p-3 md:p-4 flex justify-between items-center'>
+                    <div className='flex justify-center items-center gap-2 csm:gap-3'>
+                      <Icons.google className='w-[30px] csm:w-[48px] h-[30px] csm:h-[48px]' />
+                      <p className='text-[14px] csm:text-[18px] text-black-2 font-medium'>
+                        Found 20 reviews
+                      </p>
+                    </div>
+                    <p className='text-[14px] csm:text-[18px] text-[#777980] font-medium'>
+                      importing...
+                    </p>
+                  </div>
+                </Slide>
               </div>
             </div>
-            {/* reviews --->  */}
-            <div className='flex flex-col gap-1 w-full justify-center items-center'>
-              {/* upwork review -->  */}
-              <div className='w-full max-w-[320px] csm:max-w-[480px] md:max-w-[720px] bg-[#F3F5F7] rounded-[10px] p-2 csm:p-3 md:p-4 flex justify-between items-center'>
-                <div className='flex justify-center items-center gap-2 csm:gap-3'>
-                  <Icons.upwork className='w-[30px] csm:w-[48px] h-[30px] csm:h-[48px]' />
-                  <p className='text-[14px] csm:text-[18px] text-black-2 font-medium'>
-                    Found 20 reviews
-                  </p>
-                </div>
-                <p className='text-[14px] csm:text-[18px] text-[#777980] font-medium'>
-                  importing...
-                </p>
-              </div>
-              {/* fiver review ---> */}
-              <div className='w-full max-w-[300px] csm:max-w-[460px] md:max-w-[700px] bg-[#F3F5F7] rounded-[10px] p-3 md:p-4 flex justify-between items-center'>
-                <div className='flex justify-center items-center gap-2 csm:gap-3'>
-                  <Icons.fiver className='w-[30px] csm:w-[48px] h-[30px] csm:h-[48px]' />
-                  <p className='text-[14px] csm:text-[18px] text-black-2 font-medium'>
-                    Found 20 reviews
-                  </p>
-                </div>
-                <p className='text-[14px] csm:text-[18px] text-[#777980] font-medium'>
-                  importing...
-                </p>
-              </div>
-              {/* google review ---> */}
-              <div className='w-full max-w-[280px] csm:max-w-[440px] md:max-w-[680px] bg-[#F3F5F7] rounded-[10px] p-3 md:p-4 flex justify-between items-center'>
-                <div className='flex justify-center items-center gap-2 csm:gap-3'>
-                  <Icons.google className='w-[30px] csm:w-[48px] h-[30px] csm:h-[48px]' />
-                  <p className='text-[14px] csm:text-[18px] text-black-2 font-medium'>
-                    Found 20 reviews
-                  </p>
-                </div>
-                <p className='text-[14px] csm:text-[18px] text-[#777980] font-medium'>
-                  importing...
-                </p>
-              </div>
-            </div>
-          </div>
+          </Slide>
         </div>
       </Wrapper>
     </div>
--- app/[locale]/component/LandingPage/VerifiedReviews.tsx
+++ app/[locale]/component/LandingPage/VerifiedReviews.tsx
@@ -1,50 +1,64 @@
+'use client';
+
 import React from 'react';
 import Wrapper from '../Shared/ComponentWrapper';
 import * as Icons from '../../../../svg/Icons';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Fade, Slide, Zoom } from 'react-awesome-reveal';
 
 const VerifiedReviews: React.FC = () => {
   return (
-    <div className='bg-white-1 lg:py-0 py-10'>
-      <Wrapper>
-        <div className='w-full grid grid-cols-1 sm:gap-0 gap-4 lg:grid-cols-2 justify-center items-center'>
-          {/* left portion --->  */}
-          <div className='w-full flex flex-col gap-3 sm:gap-4 items-start pr-4'>
-            <div className='flex w-full justify-start items-start gap-2 sm:gap-4'>
-              <p className='text-[25px] sm:text-[30px] xl:text-[40px] text-black-1 font-semibold'>
-                Verified reviews.{' '}
-                <span className='uppercase text-[10px] sm:text-[15px] xl:text-[25px] text-[#534599] font-rockSalt'>
-                  once click
-                </span>
-              </p>
-              <Icons.checkIcon className='w-[25px] xl:w-[44px] h-[25px] xl:h-[55px]' />
-            </div>
-            <p className='text-[18px] xl:text-[20px] text-black-3 font-normal'>
-              Unlock the potential of your freelance careerwith our trusted
-              reviews platform. Join today and let your reputation speak
-              foritself.
-            </p>
-            <Link
-              className='text-[#0B70D6] hover:opacity-80 text-[18px] font-semibold flex justify-center items-center gap-2'
-              href='#'
-            >
-              Try it now
-              <Icons.rightArrow className='w-[24px] h-[24px]' fill='#0B70D6' />
-            </Link>
+    <Fade duration={1200} triggerOnce>
+      {' '}
+      {/* General fade effect for the container */}
+      <div className='bg-white-1 lg:py-0 py-10'>
+        <Wrapper>
+          <div className='w-full grid grid-cols-1 sm:gap-0 gap-4 lg:grid-cols-2 justify-center items-center'>
+            {/* Left portion with slide effect */}
+            <Slide direction='left' triggerOnce>
+              <div className='w-full flex flex-col gap-3 sm:gap-4 items-start pr-4'>
+                <div className='flex w-full justify-start items-start gap-2 sm:gap-4'>
+                  <p className='text-[25px] sm:text-[30px] xl:text-[40px] text-black-1 font-semibold'>
+                    Verified reviews.{' '}
+                    <span className='uppercase text-[10px] sm:text-[15px] xl:text-[25px] text-[#534599] font-rockSalt'>
+                      once click
+                    </span>
+                  </p>
+                  <Icons.checkIcon className='w-[25px] xl:w-[44px] h-[25px] xl:h-[55px]' />
+                </div>
+                <p className='text-[18px] xl:text-[20px] text-black-3 font-normal'>
+                  Unlock the potential of your freelance career with our trusted
+                  reviews platform. Join today and let your reputation speak for
+                  itself.
+                </p>
+                <Link
+                  className='text-[#0B70D6] hover:opacity-80 text-[18px] font-semibold flex justify-center items-center gap-2'
+                  href='#'
+                >
+                  Try it now
+                  <Icons.rightArrow
+                    className='w-[24px] h-[24px]'
+                    fill='#0B70D6'
+                  />
+                </Link>
+              </div>
+            </Slide>
+            {/* Right portion with slide effect */}
+            <Zoom triggerOnce delay={500}>
+              <div className='w-full h-[300px] sm:h-[400px] md:h-[500px] clg:h-[600px] relative'>
+                <Image
+                  src='/assets/verified.svg'
+                  alt=''
+                  className='object-fill'
+                  fill
+                />
+              </div>
+            </Zoom>
           </div>
-          {/* right portion ----> */}
-          <div className='w-full h-[300px] sm:h-[400px] md:h-[500px] clg:h-[600px] relative'>
-            <Image
-              src='/assets/verified.svg'
-              alt=''
-              className='object-fill'
-              fill
-            />
-          </div>
-        </div>
-      </Wrapper>
-    </div>
+        </Wrapper>
+      </div>
+    </Fade>
   );
 };
 
